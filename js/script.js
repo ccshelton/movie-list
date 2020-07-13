785ea7b37d9f2dbbb8946eff92b9d95d951c6caf
ccshelton <ccshelton@gmail.com>
2020-07-12 20:30:20 -0500

    Restyle and retheme

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('#search-submit').click(function() {
-    let input = $('#search-input-title').val();
+    let input = $('#search-title').val();
     searchMovies(input);
   })
 });
@@ -29,15 +29,15 @@ function showSearchResults(results) {
           <div class="details">
               <p class="title">${results.Title}</p>
               <ul class="rating-list">
-                  <li class="rating raing-imdb">${results.Ratings[0].Value} |</li>
-                  <li class="rating raing-rotten">${results.Ratings[1].Value} | </li>
-                  <li class="rating raing-metacritic">${results.Ratings[2].Value}</li>
+                  <li class="rating raing-imdb"><span>imdb: </span>${results.Ratings[0].Value} |</li>
+                  <li class="rating raing-rotten"><span>rotten: </span>${results.Ratings[1].Value} | </li>
+                  <li class="rating raing-metacritic"><span>meta: </span>${results.Ratings[2].Value}</li>
               </ul>
               <p class="description">${results.Plot}</p>
               <a href="#">See more</a>
           </div>
           <div class="card-actions">
-              <button class="add">Add to list</button>
+            <button class="button button-secondary">Add to list</button>
           </div>
         </div>
       `
