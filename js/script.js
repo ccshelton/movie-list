7afefe1628fff100525cd6e9cfe6af3734584c7b
ccshelton <ccshelton@gmail.com>
2020-07-20 20:41:28 -0500

    Adjust movie-card styles, add action bar to movie-card

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 
 function searchMovies() {
   let input = $('#search-title').val();
-  let key = input.split("+");
+  let key = input.split(" ").join("+");
     $.ajax({
       url: 'https://www.omdbapi.com/?apikey=1852560f&t='+key,
         data: {
@@ -33,12 +33,22 @@ function showSearchResults(results) {
           <img src="${results.Poster}" class="poster">
           <div class="details">
               <p class="title">${results.Title}</p>
-              <ul class="rating-list">
-                  <li class="rating raing-imdb"><span>imdb: </span>${results.Ratings[0].Value} |</li>
-                  <li class="rating raing-rotten"><span>rotten: </span>${results.Ratings[1].Value} | </li>
-                  <li class="rating raing-metacritic"><span>meta: </span>${results.Ratings[2].Value}</li>
+              <ul class="card-list">
+                  <li class="rating"><span>imdb: ${results.Ratings[0].Value} </span> |</li>
+                  <li class="rating"><span>rotten: ${results.Ratings[1].Value} </span> | </li>
+                  <li class="rating"><span>meta: ${results.Ratings[2].Value}</span></li>
               </ul>
-              <p class="description">${results.Plot}</p>
+              <p class="description">${results.Plot}</p>              
+              <ul class="card-list">
+                <li>released: ${results.Year} | </li>
+                <li>runtime: ${results.Runtime}</li>
+              </ul>
+              <div class="action-bar">
+                <button class="button button-primary">Add to list</button>
+                <a href="https://www.youtube.com/results?search_query=${results.Title.split(" ").join("+")}" target="_blank">
+                  <button class="button button-secondary">Search Youtube</button>
+                </a>
+              </div>
           </div>
         </div>
       `
